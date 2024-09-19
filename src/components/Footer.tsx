51140c58ec76900cf0b4245c225a85a28a164a6e
cjfromthez <144188759+cjfromthez@users.noreply.github.com>
2024-09-19 15:25:50 -0400

    test

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,6 +9,7 @@ function Footer() {
       <div>
       <a href="https://github.com/Stoplossking1" target="_blank" rel="noreferrer"><GitHubIcon/></a>
       <a href="https://www.linkedin.com/in/jordan-hodali-57a80a238/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+      <p> Thanks Yuji!</p>
       </div>
     </footer>
   );
